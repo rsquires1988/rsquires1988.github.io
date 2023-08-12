2c7614069e85b190daaf1c07f854ff473da5340d
rsquires1988 <ryguy88enator@gmail.com>
2023-08-12 21:53:22 +0000

    fix links, add socials

--- pelicanconf.py
+++ pelicanconf.py
@@ -19,12 +19,11 @@ AUTHOR_FEED_RSS = None
 # Blogroll
 LINKS = (('Pelican', 'https://getpelican.com/'),
          ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'))
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('GitHub', 'https://rsquires1988.github.com'),
+          ('LinkedIn', 'https://www.linkedin.com/in/ryan-squires-0a15841b0/'))
 
 DEFAULT_PAGINATION = 10
 
