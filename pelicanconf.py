51081fe577552f8983cd1d3aa74c572025ce64e0
rsquires1988 <ryguy88enator@gmail.com>
2023-08-13 21:28:19 +0000

    try thumb

--- pelicanconf.py
+++ pelicanconf.py
@@ -44,6 +44,30 @@ IMAGE_PROCESS = {
     },
     "article-image": {
         "type": "image",
-        "ops": ["scale_in 280 280 True"],
+        "ops": ["scale_in 300 300 True"],
+    },
+    "crisp": {
+        "type": "responsive-image",
+        "srcset": [
+            ("1x", ["scale_in 800 600 True"]),
+            ("2x", ["scale_in 1600 1200 True"]),
+            ("4x", ["scale_in 3200 2400 True"]),
+        ],
+        "default": "1x",
+    },
+    "large-photo": {
+        "type": "responsive-image",
+        "sizes": (
+            "(min-width: 1200px) 800px, "
+            "(min-width: 992px) 650px, "
+            "(min-width: 768px) 718px, "
+            "100vw"
+        ),
+        "srcset": [
+            ("600w", ["scale_in 600 450 True"]),
+            ("800w", ["scale_in 800 600 True"]),
+            ("1600w", ["scale_in 1600 1200 True"]),
+        ],
+        "default": "800w",
     },
 }
